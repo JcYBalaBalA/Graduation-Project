3d45205532c5dfe356528247f02deb86f7640ee0
JcYBalaBalA <37809981+JcYBalaBalA@users.noreply.github.com>
2021-03-30 21:52:42 +0800

    Add files via upload

--- attack/black_box.py
+++ attack/black_box.py
@@ -1,6 +1,7 @@
-
-import torch
+import torch,os
 import train.models as mod
+import attack.white_box as wb
+
 # 黑盒攻击
 # 可了解信息：
 # 1.何种数据集
@@ -9,9 +10,73 @@ import train.models as mod
 
 # 输入：
 # GenPath：可访问到的生成模型路径 string
-# aux_trainloader：作为辅助信息的train dataloader dataloader default = None
-# aux_testloader：作为辅助信息的test dataloader dataloader default = None
+# aux_trainloader：作为辅助信息的train集 dataloader default = None
+# aux_testloader：作为辅助信息的test集 dataloader default = None
 # modName：需要使用何种模型攻击 string default = "dcgan"
 # attack_modPath：训练出的攻击模型保存路径 string
-def attack(modName, opt, modPath, imgPath, dataloader=None, aimGen=None,
-           aux_trainloader=None, aux_testloader=None):
+def attack(attack_modName, opt_list, savepath, dataloader, GenPath,
+           aux_trainloader=None, aux_testloader=None, cuda=False):
+    modPath = savepath + "/models"
+    imgPath = savepath + "/images"
+
+    os.makedirs(savepath, exist_ok=True)
+    os.makedirs(savepath + "/images", exist_ok=True)
+    os.makedirs(savepath + "/models", exist_ok=True)
+
+    opt = mod.model_opt(opt_list)
+
+    aimGen = torch.load(GenPath)
+    if cuda:
+        aimGen.cuda()
+
+    if attack_modName == "began":
+        began = mod.began()
+        began.train(opt, modPath, imgPath, dataloader=aux_trainloader,
+                    aimGen=aimGen, aux_testloader=aux_testloader)
+    elif attack_modName == "dcgan":
+        dcgan = mod.dcgan()
+        dcgan.train(opt, modPath, imgPath, dataloader=aux_trainloader,
+                    aimGen=aimGen, aux_testloader=aux_testloader)
+
+    # 用黑盒攻击生成的每一个分类器模型进行成员推理检测
+    rs = {}
+    D_list = os.listdir(modPath)
+    for D in D_list:
+        if D.startswith("netD"):
+            rs[int(D.split("_")[-1])] = wb.attack(dataloader, len(dataloader), modPath + "\\" + D)
+    return rs
+
+# 实验用
+def attack_show(attack_modName, opt_list, savepath, GenPath, trainloader, testloader,
+           aux_trainloader=None, aux_testloader=None, cuda=False):
+    modPath = savepath + "/models"
+    imgPath = savepath + "/images"
+
+    os.makedirs(savepath, exist_ok=True)
+    os.makedirs(savepath + "/images", exist_ok=True)
+    os.makedirs(savepath + "/models", exist_ok=True)
+
+    opt = mod.model_opt(opt_list)
+    print(attack_modName + "_opt:")
+    opt.list_all_member()
+
+    aimGen = torch.load(GenPath)
+    if cuda:
+        aimGen.cuda()
+
+    if attack_modName == "began":
+        began = mod.began()
+        began.train(opt, modPath, imgPath, dataloader=aux_trainloader,
+                    aimGen=aimGen, aux_testloader=aux_testloader)
+    elif attack_modName == "dcgan":
+        dcgan = mod.dcgan()
+        dcgan.train(opt, modPath, imgPath, dataloader=aux_trainloader,
+                    aimGen=aimGen, aux_testloader=aux_testloader)
+
+    # 用黑盒攻击生成的每一个分类器模型进行成员推理检测
+    rs = {}
+    D_list = os.listdir(modPath)
+    for D in D_list:
+        if D.startswith("netD"):
+            rs[int(D.split("_")[-1])] = wb.attack_show(trainloader, testloader, len(trainloader), modPath + "\\" + D)
+    return rs
--- attack/white_box.py
+++ attack/white_box.py
@@ -7,43 +7,32 @@ import torch
 # 3.可以访问攻击对象的分类器
 # 预留两种接口
 # 1.传入disPath，路径所代表的是通过torch.save(net,PATH)保存的模型
-# 2.传入disPath、disMod，路径所代表的是通过torch.save(net.state_dict,PATH)保存的参数
-# 而disMod代表具有某些参数的目标模型，通过disMod.load_state_dict载入这些参数
-# 3.传入disMod，直接传入所要攻击的模型
+# 2.disState，所代表的是通过torch.save(net.state_dict,PATH)保存的参数
+# 而disMod代表具有某些参数的目标模型，通过
+# load_state_dict载入这些参数
+# 3.传入disMod，直接传入所要攻击模型的路径
 
 # 传入的trainloader及testloader仅代表获取数据时默认的训练集和测试集,若只需要一个输入可将testloader置为None
 # return 长度为trainsize的样本数组
-def attack(trainloader, testloader, trainsize, cuda, disPath = None, disMod = None):
-    if disPath != None and disMod == None:
-        disMod = torch.load(disPath)
-    elif disPath != None and disMod != None:
-        disMod = disMod.load_state_dict(torch.load(disPath))
+def attack(dataloader, trainsize, disMod, cuda=False):
+    disMod = torch.load(disMod)
 
     if cuda:
         disMod.cuda()
 
     wb_predictions = []
-    for i, (img, _) in enumerate(trainloader, 0):
+    for i, (img, _) in enumerate(dataloader, 0):
         output = disMod(img)
         output = [x for x in output.detach().cpu().numpy()]
         output = list(zip(output, [img[i] for i in range(len(output))]))
         wb_predictions.extend(output)
-    if testloader != None:
-        for i, (img, _) in enumerate(testloader, 0):
-            output = disMod(img)
-            output = [x for x in output.detach().cpu().numpy()]
-            output = list(zip(output, [img[i] for i in range(len(output))]))
-            wb_predictions.extend(output)
 
     return [x[1] for x in sorted(wb_predictions, reverse=True)[:len(trainsize)]]
 
 # 实验用
 # return 白盒攻击预测准确度
-def attack_show(trainloader, testloader, trainsize, cuda, disPath = None, disMod = None):
-    if disPath != None and disMod == None:
-        disMod = torch.load(disPath)
-    elif disPath != None and disMod != None:
-        disMod = disMod.load_state_dict(torch.load(disPath))
+def attack_show(trainloader, testloader, trainsize, disMod, cuda=False):
+    disMod = torch.load(disMod)
 
     if cuda:
         disMod.cuda()
